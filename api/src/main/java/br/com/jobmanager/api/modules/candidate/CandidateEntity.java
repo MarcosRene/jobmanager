ca21c463d9db8d6cd5ebe88643aaadd03cb41f8d
Marcos Rene <renemoura789@gmail.com>
2025-02-19 20:26:58 -0300

    feat: add candidate initial controller/entity

---
+++ api/src/main/java/br/com/jobmanager/api/modules/candidate/CandidateEntity.java
@@ -0,0 +1,26 @@
+package br.com.jobmanager.api.modules.candidate;
+
+import java.util.UUID;
+
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+@Data
+public class CandidateEntity {
+  private UUID id;
+  private String name;
+
+  @Pattern(regexp = "^(?!\\s*$).+", message = "o campo [username] não deve conter espaço") 
+  private String username;
+
+  @Email(message = "o campo [email] deve conter um e-mail válido")
+  private String email;
+
+  @Length(min = 8)
+  private String password;
+  private String description;
+  private String curriculum;
+}
