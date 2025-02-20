e244388b1e807d2b9e702d8abe902f7404c7133a
Marcos Rene <renemoura789@gmail.com>
2025-02-20 09:43:30 -0300

    feat: add candidate table

--- api/src/main/java/br/com/jobmanager/api/modules/candidate/CandidateEntity.java
+++ api/src/main/java/br/com/jobmanager/api/modules/candidate/CandidateEntity.java
@@ -1,18 +1,30 @@
 package br.com.jobmanager.api.modules.candidate;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+@Entity(name = "candidate")
 public class CandidateEntity {
+  @Id()
+  @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
+  
   private String name;
   
+  @NotBlank()
   @Pattern(regexp = "\\S+", message = "O campo [username] não deve conter espaço") 
   private String username;
 
@@ -21,6 +33,11 @@ public class CandidateEntity {
 
   @Length(min = 8, max = 20, message = "A senha deve conter entre (8) e (20) caracteres")
   private String password;
+  
   private String description;
+  
   private String curriculum;
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
 }
